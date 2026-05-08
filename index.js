8af79edb7a4a3e95052e9f67f3fb7defdccba47e
ayusshh66 <adonisjefrey66@gmail.com>
2026-05-09 02:03:49 +0530

    added ZOD for validating incoming users for signup and login

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import userRouter from './routes/users.routes.js'
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
+app.use(express.json())
+
 app.get('/', (req,res) => {
     res.status(200).end('server is ready ')
 })
+
+app.use('/user', userRouter);
 
 app.listen(PORT, () => {
     console.log(`server is up and running at port number : ${PORT}`)
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,14 +1,21 @@
 import express from 'express'
 import db from '../src/index.js';
 import { usersTable } from '../models/user.model.js';
+import {signupPostRequestBodySchema} from '../validation/request.validation.js'
 import { eq } from 'drizzle-orm';
 import { randomBytes, createHmac } from 'node:crypto';
 
 const route = express.Router();
 
-route.get('/signin', async (req,res) => {
+route.post('/signup', async (req,res) => {
     // req= user's want (request), body = what users enter in below field
-    const {firstname, lastname, email, password} =  req.body;
+    const validationResult = await signupPostRequestBodySchema.safeParseAsync(req.body);
+
+    if(validationResult.error){
+        return res.status(400).json({error : validationResult.error.format()})
+    }
+
+    const {firstname, lastname, email, password} = validationResult.data;
 
     // checks the email entered with userstable email
     const [exisitingUser] = await db.select({id: usersTable.id}).from(usersTable).where(eq(usersTable.email,email));
@@ -22,7 +29,7 @@ route.get('/signin', async (req,res) => {
 
     //fills the value given by the user in userstable data
     const [user] = await db.insert(usersTable).values({
-        id,
+        // id,
         firstname,
         lastname,
         email,
