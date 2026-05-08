98444dec9082960278d74d458a0b9ed9686bb5ae
ayusshh66 <adonisjefrey66@gmail.com>
2026-05-08 23:24:35 +0530

    designed and implemented the user schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,19 @@
-import {pgTable, uuid} from 'drizzle-orm/pg-core'
+// import { varchar } from 'drizzle-orm/mysql-core'
+// import { date } from 'drizzle-orm/mysql-core'
+import {pgTable, uuid,varchar,text, timestamp} from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users', {
     id : uuid().primaryKey().defaultRandom(),
-    //..
+
+    firstname : varchar("First_Name",{length : 55}).notNull(),
+    lastname : varchar("Last_Name", {length : 55}),
+
+    email : text().notNull(),
+
+    password : text().notNull(),
+    salt : text().notNull(),
+    
+    createdAt : timestamp('created_at').defaultNow().notNull(),
+    updatedAt : timestamp("Updated_at").$onUpdate(()=> new Date())
+
 })
