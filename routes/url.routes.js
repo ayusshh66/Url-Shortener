8ecd34168b70c929ff5bc4ab806e46576eae6231
ayusshh66 <adonisjefrey66@gmail.com>
2026-05-10 00:53:14 +0530

    made working url shortener FINALLY!!!!

--- routes/url.routes.js
+++ routes/url.routes.js
@@ -5,6 +5,7 @@ import {ensureAuthentication} from '../middlewares/auth.middleware.js'
 import {createShortenUrl} from '../services/url.service.js'
 import db from '../src/index.js'
 import {nanoid} from 'nanoid'
+import { eq } from 'drizzle-orm'
 
 const route = express.Router();
 
@@ -21,6 +22,16 @@ route.post('/shorten',ensureAuthentication, async(req,res) => {
     const result = await createShortenUrl({shortCode, targetUrl : url, userId});
 
     return res.status(201).json({ id : result.id, shortCode : result.shortCode, url : result.targetUrl})
+})
+
+route.get('/:shortCode', async (req,res) =>{
+    const code = req.params.shortCode;
+
+    const [result] = await db.select({targetUrl : urlsTable.targetUrl}).from(urlsTable).where(eq(urlsTable.shortCode, code));
+
+    if(!result) return res.status(400).json({message : `INVALID URL`});
+
+    return res.redirect(result.targetUrl)
 })
 
 export default route;
