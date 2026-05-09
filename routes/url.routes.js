d8b77665f108f71c7ae33a9f259d004bc25ca841
ayusshh66 <adonisjefrey66@gmail.com>
2026-05-10 03:32:45 +0530

    method to delete urls and ownership with /and/

--- routes/url.routes.js
+++ routes/url.routes.js
@@ -5,7 +5,7 @@ import {ensureAuthentication} from '../middlewares/auth.middleware.js'
 import {createShortenUrl} from '../services/url.service.js'
 import db from '../src/index.js'
 import {nanoid} from 'nanoid'
-import { eq } from 'drizzle-orm'
+import { and, eq } from 'drizzle-orm'
 
 const route = express.Router();
 
@@ -24,10 +24,18 @@ route.post('/shorten',ensureAuthentication, async(req,res) => {
     return res.status(201).json({ id : result.id, shortCode : result.shortCode, url : result.targetUrl})
 })
 
-route.get('/codes', async (req,res) => {
+route.get('/codes', ensureAuthentication,async (req,res) => {
     const codes = await db.select().from(urlsTable).where(eq(urlsTable.userId,req.user.id));
 
     return res.status(200).json({codes })
+})
+
+route.delete('/:id',ensureAuthentication, async (req,res) => {
+    const id = req.params.id;
+
+    const result = await db.select().from(urlsTable).where(and(eq(urlsTable.id,id), eq(urlsTable.user, req.user.id))) //ensures 2 verification
+
+    return res.status(200).json({delete : "true"})
 })
 
 route.get('/:shortCode', async (req,res) =>{
