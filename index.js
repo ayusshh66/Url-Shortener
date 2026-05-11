d0a728ca1db7d54512bf83f34e02f9fd73649fa1
ayusshh66 <adonisjefrey66@gmail.com>
2026-05-11 20:45:25 +0530

    set up frontend for url shortener

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import express from 'express';
 import userRouter from './routes/users.routes.js'
 import {authenticationMiddleware} from './middlewares/auth.middleware.js'
 import urlRouter from './routes/url.routes.js'
+import cors  from 'cors'
 import 'dotenv'
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
+app.use(cors())
 app.use(express.json())
 app.use(authenticationMiddleware)
+
+app.get('/intro', (req,res) => {
+    return res.send('yooo what is up')
+})
 
 app.get('/', (req,res) => {
     res.status(200).end('server is ready ')
@@ -16,6 +22,8 @@ app.get('/', (req,res) => {
 
 app.use('/user', userRouter);
 app.use('/',urlRouter)
+
+
 
 app.listen(PORT, () => {
     console.log(`server is up and running at port number : ${PORT}`)
